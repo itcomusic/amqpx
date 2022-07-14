359555d7cbc6e0c12e572815eac0e1c2a2adc6ea
Andrey Nabiullin <itcomusic@gmail.com>
2022-07-14 21:45:42 +0300

    chore(log): default log

--- log.go
+++ log.go
@@ -1,7 +1,19 @@
 package amqpx
 
+import (
+	"log"
+	"os"
+)
+
 // LogFunc type is an adapter to allow the use of ordinary functions as LogFunc.
 type LogFunc func(format string, args ...any)
 
 // NoopLogger logFunc does nothing
 var NoopLogger = LogFunc(func(_ string, _ ...any) {})
+
+var defaultLogger = func() LogFunc {
+	logger := log.New(os.Stderr, "amqpx: ", log.LstdFlags)
+	return func(format string, args ...any) {
+		logger.Printf(format, args...)
+	}
+}()
